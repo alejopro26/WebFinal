cfb1e7f568eda067697daf9086cc8a7016081095
Alejandro <alejo.puentes.salazar@gmail.com>
2025-11-23 09:44:18 -0500

    Cambios Nuevos 2

---
+++ frontend/chat-frontend/tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,jsx,ts,tsx}",
+        // Mantine usa Emotion, Tailwind no lo procesa, pero lo dejamos
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                discord: {
+                    900: '#0f1720', // panel fondo
+                    800: '#121318',
+                    700: '#1f1f29',
+                    blurple: '#5865F2',
+                    violet: '#7c5cff',
+                    bg: '#0f1115',
+                    card:'#1e1f23'
+                }
+            },
+            fontFamily: {
+                sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+            }
+        },
+    },
+    plugins: [],
+}
